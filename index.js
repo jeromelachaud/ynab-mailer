006c2e3bccbe8ecb816ac9d5d1cd2960ced2cfd2
Jerome Lachaud <jerome.lachaud@gmail.com>
2019-04-19 13:40:33 +0200

    Refactor ATT
    Add monthly email

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /* eslint-disable indent */
 require('dotenv').config()
-
 const schedule = require('node-schedule')
-const app = require('./src/app')
+
+const fetchTransactions = require('./lib/fetchTransactions')
+const mailer = require('./lib/mailer')
+
+const app = async () => {
+  const transactions = await fetchTransactions()
+  mailer(transactions)
+}
 process.env.NODE_ENV !== 'development'
   ? schedule.scheduleJob({ hour: 10, minute: 0, second: 0 }, () => {
       app()
