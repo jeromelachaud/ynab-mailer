fbf1eb91408b3d30aaa8f20be554e8072661429d
Jerome Lachaud <jerome.lachaud@gmail.com>
2019-04-20 17:56:05 +0200

    Add monthly email

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
 require('dotenv').config()
 const schedule = require('node-schedule')
 
-const fetchTransactions = require('./lib/fetchTransactions')
-const mailer = require('./lib/mailer')
+const fetchTransactions = params => require('./lib/fetch')(params)
+const mailTransactions = (transactions, template) =>
+  require('./lib/mailer')(transactions, template)
 
-const app = async () => {
-  const transactions = await fetchTransactions()
-  mailer(transactions)
-}
-process.env.NODE_ENV !== 'development'
-  ? schedule.scheduleJob({ hour: 10, minute: 0, second: 0 }, () => {
-      app()
-    })
-  : app()
+const app =
+  process.env.NODE_ENV !== 'development'
+    ? async () => {
+        schedule.scheduleJob({ hour: 10, minute: 0, second: 0 }, async () => {
+          const transactions = await fetchTransactions('sinceYesterday')
+          mailTransactions(transactions, 'daily')
+        })
+        schedule.scheduleJob('1 10 1 * *', async () => {
+          const transactions = await fetchTransactions(
+            'sinceFirstDateOfThePreviousMonth'
+          )
+          mailTransactions(transactions, 'monthly')
+        })
+      }
+    : async () => {
+        const transactions = await fetchTransactions('sinceYesterday')
+        mailTransactions(transactions, 'monthly')
+      }
+
+app()
