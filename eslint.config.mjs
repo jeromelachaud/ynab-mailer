cbc72b753b5bad5dbbf278778ed6ff5cb588ee2a
Jerome Lachaud <jerome.lachaud@gmail.com>
2025-10-26 15:38:49 +0100

    chore: update devDependencies for ESLint and add globals package

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "eslint/config";
+import promise from "eslint-plugin-promise";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended"),
+
+    plugins: {
+        promise,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        ecmaVersion: 2017,
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "never"],
+        "no-console": "off",
+        "promise/catch-or-return": "error",
+    },
+}]);
